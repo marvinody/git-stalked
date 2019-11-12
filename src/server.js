089638e34c4c10d914594608a47d6216b2c8baf7
Manuel Bugallo <marvinody@gmail.com>
2019-11-12 16:25:32 -0500

    feat: gets basic in-memory sessions working

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import * as sapper from '@sapper/server';
 import compression from 'compression';
 import dotenv from 'dotenv';
 import express from 'express';
+import session from 'express-session';
+import morgan from 'morgan';
 import sirv from 'sirv';
 dotenv.config()
 const { PORT, NODE_ENV } = process.env;
@@ -9,6 +11,12 @@ const dev = NODE_ENV === 'development';
 
 express() // You can also use Express
 	.use(
+		morgan('dev'),
+		session({
+			secret: process.env.SESSION_SECRET || 'i like turtles',
+			resave: false,
+			saveUninitialized: false
+		}),
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
