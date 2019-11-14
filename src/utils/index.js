e719e4e8e7065363f9fbee290cf68bfe56c2a8f1
Manuel Bugallo <marvinody@gmail.com>
2019-11-13 20:55:24 -0500

    feat: adds getRepo func. to github api client-side

--- src/utils/index.js
+++ src/utils/index.js
@@ -19,5 +19,17 @@ export const GithubApi = (token) => {
         return false;
       }
     },
+    getRepos: async (username) => {
+      try {
+        // grab all their repos
+        const { data: repos } = await ax.get(`users/${username}/repos`);
+        // and just give them
+        return repos
+      } catch (err) {
+        // otherwise no good
+        console.error(err)
+        return []
+      }
+    }
   }
 }
