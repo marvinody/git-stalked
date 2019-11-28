3f4d4ac68f89e72ec8a3b6e0f7f79e1082de34ca
Manuel Bugallo <marvinody@gmail.com>
2019-11-27 20:25:21 -0500

    refactor: changes process key name

--- src/routes/auth/github/index.js
+++ src/routes/auth/github/index.js
@@ -1,8 +1,7 @@
-
 export function get(req, res, next) {
   const url = 'https://github.com/login/oauth/authorize?' + [
     `client_id=${process.env.GITHUB_CLIENT_ID}`,
-    `redirect_uri=${process.env.GITHUB_DEV_REDIRECT}`
+    `redirect_uri=${process.env.GITHUB_REDIRECT}`
   ].join('&')
   res.redirect(url)
 }
