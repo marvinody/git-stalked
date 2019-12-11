07e5646e57c478b4a91c284a0410ae2da0d48f0d
Manuel Bugallo <marvinody@gmail.com>
2019-12-10 23:06:59 -0500

    feat: email fetcher caches results for some configurable time

--- src/routes/git/[user]/[repo].json.js
+++ src/routes/git/[user]/[repo].json.js
@@ -1,8 +1,17 @@
-import axios from 'axios'
-import { exec } from 'child-process-promise'
-import { Email, Repo } from '../../../db'
-const GIT_ERROR_MSG = 'Invalid repo or user (or insufficient permissions)'
-const SQL_ERROR_MSG = 'Oops, something broke! try again later'
+import axios from "axios"
+import { exec } from "child-process-promise"
+import { Email, Repo } from "../../../db"
+const GIT_ERROR_MSG = "Invalid repo or user (or insufficient permissions)"
+const SQL_ERROR_MSG = "Oops, something broke! try again later"
+
+const SECOND = 1000
+const MINUTE = SECOND * 60
+const HOUR = MINUTE * 60
+const DAY = HOUR * 24
+const WEEK = DAY * 7
+
+// when do we refresh the saved emails?
+const REFRESH_PERIOD = 1 * DAY
 
 const is_valid_repo = async (user, repo) => {
   try {
@@ -13,14 +22,16 @@ const is_valid_repo = async (user, repo) => {
   }
 }
 
-const get_emails_from_repo = async (repo_path) => {
-  const delim = ' - '
-  const cmd = await exec(`cd ${repo_path} && git log --pretty=format:"%an${delim}%ae" | sort | uniq`)
+const get_emails_from_repo = async repo_path => {
+  const delim = " - "
+  const cmd = await exec(
+    `cd ${repo_path} && git log --pretty=format:"%an${delim}%ae" | sort | uniq`
+  )
   if (cmd.stderr) throw cmd.stderr
 
   const emails = cmd.stdout
     .trim()
-    .split('\n')
+    .split("\n")
     .map(line => {
       const [name, email] = line.split(delim)
       return { name, email }
@@ -30,7 +41,9 @@ const get_emails_from_repo = async (repo_path) => {
 }
 
 const download_repo = (user, repo, repo_path) => {
-  return exec(`git clone --bare https://github.com/${user}/${repo} ${repo_path}`)
+  return exec(
+    `git clone --bare https://github.com/${user}/${repo} ${repo_path}`
+  )
 }
 
 const mktemp_dir = async () => {
@@ -38,27 +51,22 @@ const mktemp_dir = async () => {
   return dir.trim()
 }
 
-const remove_repo = (repo_path) => {
+const remove_repo = repo_path => {
   return exec(`rm -rf ${repo_path}`)
 }
 
-const send_new_emails = async (user, repo, res) => {
+const fetch_emails = async (user, repo) => {
   const repo_path = await mktemp_dir()
   try {
-    if (!await is_valid_repo(user, repo.name)) {
-      throw new Error('404')
+    if (!(await is_valid_repo(user, repo.name))) {
+      throw new Error("404")
     }
     await download_repo(user, repo.name, repo_path)
 
     const raw_emails = await get_emails_from_repo(repo_path)
-
-    const emails = await Email.bulkCreate(raw_emails)
-    repo.setEmails(emails)
-
-    res.json(raw_emails)
+    return raw_emails
   } catch (err) {
-    console.error(err)
-    res.status(404).send(GIT_ERROR_MSG)
+    throw err
   } finally {
     remove_repo(repo_path)
   }
@@ -66,38 +74,63 @@ const send_new_emails = async (user, repo, res) => {
 
 export async function get(req, res) {
   const { user, repo: repo_name } = req.params
-  if (user.includes('.') || user.includes('/')) {
+  if (user.includes(".") || user.includes("/")) {
     res.status(403).send(GIT_ERROR_MSG)
     return
   }
 
-  if (repo_name.includes('.') || repo_name.includes('/')) {
+  if (repo_name.includes(".") || repo_name.includes("/")) {
     res.status(403).send(GIT_ERROR_MSG)
     return
   }
   try {
     const [repo, created] = await Repo.findOrCreate({
       where: {
-        user, name: repo_name,
+        user,
+        name: repo_name,
       },
     })
+    const now = new Date()
+    const diff = now - repo.updatedAt
+    const youngerThanRefreshPeriod = diff < REFRESH_PERIOD
+    console.log({
+      created,
+      youngerThanRefreshPeriod,
+      diff,
+      repo: repo.dataValues,
+    })
 
-    // did it exist before? Maybe just return the emails we have already
-    if (!created) {
+    // did it exist before?
+    // if yes, has it been a short time since we updated it?
+    // Maybe just return the emails we have already
+    if (!created && youngerThanRefreshPeriod) {
       const emails = await repo.getEmails()
-      res.json(emails.map(email => ({
-        name: email.name,
-        email: email.email,
-      })))
+      res.json(
+        emails.map(email => ({
+          name: email.name,
+          email: email.email,
+        }))
+      )
       return
     }
-    // if it hasn't existed, continue and fetch new stuff
-    send_new_emails(user, repo, res)
+    // make sure it updates 'updatedAt' col.
+    // need to set this to curr time for future calls
+    repo.changed("updatedAt", true)
+    await repo.save()
 
+    // if it hasn't existed, continue and fetch new stuff
+    const raw_emails = await fetch_emails(user, repo)
+    const emails = await Email.bulkCreate(raw_emails)
+    repo.setEmails(emails)
+
+    res.json(
+      emails.map(email => ({
+        name: email.name,
+        email: email.email,
+      }))
+    )
   } catch (err) {
     console.error(err)
     res.status(500).end(SQL_ERROR_MSG)
   }
-
 }
-
