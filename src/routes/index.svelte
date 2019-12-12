7902a2dbe49c23c8524ed9993f24eba23f9eb6a4
Manuel Bugallo <marvinody@gmail.com>
2019-12-12 00:15:09 -0500

    feat: updates main index with relevant info

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <style>
   h1,
-  figure,
   p {
-    text-align: center;
     margin: 0 auto;
+  }
+
+  .center {
+    text-align: center;
   }
 
   h1 {
@@ -13,18 +15,12 @@
     margin: 0 0 0.5em 0;
   }
 
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
   p {
     margin: 1em auto;
+    text-align: left;
+  }
+  ul {
+    list-style-type: disclosure-closed;
   }
 
   @media (min-width: 480px) {
@@ -38,15 +34,48 @@
   <title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1 class="center">Git Stalked!</h1>
 
-<figure>
-  <img alt="Borat" src="great-success.png" />
-  <figcaption>HIGH FIVE!</figcaption>
-</figure>
+<p class="center">
+  <strong>An experiment in being creepy.</strong>
+</p>
 
 <p>
-  <strong>
-    Try editing this file (src/routes/index.svelte) to test live reloading.
-  </strong>
+  At some point, I realized git commits track an author's emails and then at
+  another point I had the idea of being creepy. One night both of these ideas
+  together and formed an even worse fused idea. What if you could type in
+  someone's name and get their email and anyone else they worked with's email.
+  And thus, this was born.
+</p>
+
+<p>
+  This is a really crude app in the way I did out (source over
+  <a href="https://github.com/marvinody/git-stalked">here</a>
+  if you're into that sorta thing) but it was pretty fun to make in a more
+  Svelte-y way than some of my previous Svelte projects.
+</p>
+
+<p>Pretty simple execution:</p>
+<ul>
+  <li>User enters a username</li>
+  <li>Search up public repos owned by that username</li>
+  <li>For each repo:</li>
+  <ul>
+    <li>Download it</li>
+    <li>Get all the commits' authors and find only unique entries</li>
+  </ul>
+  <li>Delete the repo</li>
+</ul>
+<p>
+  All of the above gets cached for each repo for some time so Github doesn't
+  yell at me too much.
+</p>
+<p>
+  You might be wondering why Github Oauth is required if it doesn't even allow
+  you to see private repos. I don't lookup private repos for privacy concerns
+  since it's cached. So why not require it at all? Github has a low limit on
+  username search (like 10/min) without auth. So your authentication is to just
+  allow you to search up a bunch of people and have nice immediate search on the
+  form. That's it. Use a dummy account if you want and nothing will be
+  different!
 </p>
