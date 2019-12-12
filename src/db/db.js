eeff31d750ac3baf2ac7b8a14d0948174bc70c22
Manuel Bugallo <marvinody@gmail.com>
2019-12-11 20:30:08 -0500

    feat: removes dropping db every time it runs

--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,10 @@
-import Sequelize from 'sequelize';
+import Sequelize from "sequelize"
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/git-stalked`,
   {
-    logging: false
+    logging: false,
   }
-);
-
-db.sync({ force: true })
+)
 
 export default db
